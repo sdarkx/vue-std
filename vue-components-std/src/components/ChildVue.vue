0942ee9767656e4d198fb2fadd1dd9a93b96361a
DARK <3522954997@qq.com>
2023-08-05 15:06:25 +0800

    props数据传递

--- vue-components-std/src/components/ChildVue.vue
+++ vue-components-std/src/components/ChildVue.vue
@@ -3,12 +3,22 @@
         <h3>Child</h3>
         <div>
             <p>{{ heritage }}</p>
+            <p>{{ age }}</p>
+        </div>
+        <div>
+            <ul>
+                <li v-for="(val, index) of names" :key="index">{{ val }}</li>
+            </ul>
+        </div>
+        <div>
+            <p>Name: {{ userInfo.name }}</p>
+            <p>Phone: {{ userInfo.phone }}</p>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ["heritage"],
+    props: ["heritage", "age", "names", "userInfo"],
 }
 </script>
--- vue-components-std/src/components/ParentVue.vue
+++ vue-components-std/src/components/ParentVue.vue
@@ -2,7 +2,7 @@
     <div>
         <h2>Parent</h2>
         <div>
-            <ChildVue :heritage="msg"/>
+            <ChildVue :heritage="msg" :age="age" :names="names" :userInfo="userInfo"/>
         </div>
     </div>
 </template>
@@ -13,7 +13,13 @@ import ChildVue from './ChildVue.vue'
 export default {
     data(){
         return {
-            msg: "123123"
+            msg: "123123",
+            age: 100,
+            names:['a', 'c', 'd', '123'],
+            userInfo: {
+                name: "tanphgo",
+                phone: "18200000000",
+            }
         }
     },
     components: {
